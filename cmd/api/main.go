9cd6a688496cb4074032563f0dfa0e181cb4ff51
Plastic Existence <ozim_ssspam@mail.ru>
2025-02-11 19:03:47 +0300

    feat: add graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,14 +2,48 @@ package main
 
 import (
 	"avitotech/internal/server"
+	"context"
+	"errors"
+	"fmt"
 	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
+func gracefulShutdown(apiServer *http.Server, done chan bool) {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	<-ctx.Done()
+
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := apiServer.Shutdown(ctx); err != nil {
+		log.Printf("Server forced to shutdown with error: %v", err)
+	}
+
+	log.Println("Server exiting")
+
+	done <- true
+}
+
 func main() {
+
 	srv := server.NewServer()
 
+	done := make(chan bool, 1)
+
+	go gracefulShutdown(srv, done)
+
 	err := srv.ListenAndServe()
-	if err != nil {
-		log.Fatalf("http server error: %s", err)
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		panic(fmt.Sprintf("http server error: %s", err))
 	}
+
+	<-done
+	log.Println("Graceful shutdown complete.")
 }
