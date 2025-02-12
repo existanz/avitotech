23ab232a4686768a7d7ec43b5658ac1c089035f3
Plastic Existence <ozim_ssspam@mail.ru>
2025-02-12 12:11:54 +0300

    feat: add cors rules to gin

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -3,11 +3,19 @@ package server
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"}, // My frontend URL
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type"},
+		AllowCredentials: true,
+	}))
 
 	r.GET("/", s.BaseHandler)
 
